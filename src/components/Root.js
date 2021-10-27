934f2961857a09ce50bb6f54c81956de0b2e33f6
birodaniel8 <birodaniel8@gmail.com>
2021-10-28 00:17:59 +0200

    create react app and setup properly

---
+++ src/components/Root.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux"; // wrap this around everything for redux
+
+const Root = () => {
+  return (
+    <div>
+      <p>HomePage</p>
+    </div>
+  );
+};
+
+export default Root;
